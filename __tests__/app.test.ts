44f6433f3bbd333dcdc045e117109480fc500d4c
Liran Tal <liran.tal@gmail.com>
2024-12-23 17:15:54 +0200

    feat: add initial program logic

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -1,8 +1,8 @@
 import { test, describe, beforeEach, mock } from 'node:test'
 import assert from 'node:assert'
-import { sheriff } from '../src/main.ts'
+import { UrlSheriff } from '../src/main.ts'
 
-describe('SSRF program API', () => {
+describe('SSRF Private Hosts Test Suite', () => {
 
   beforeEach(() => {
     mock.reset()
@@ -10,14 +10,36 @@ describe('SSRF program API', () => {
 
   test('If a URL uses a host set to a localhost IP address, an exception is thrown', async (t) => {
 
+    const sheriff = new UrlSheriff({})
+
     await assert.rejects(
-      sheriff('http://localhost:3000'),
+      sheriff.isSafe('http://127.0.0.1:3000'),
       {
         name: 'Error',
-        message: 'URL uses a host set to a localhost IP'
+        message: 'URL uses a private hostname'
       }
     )
 
+  })
+
+  test('If a URL uses a host set to a localhost domain, an exception is thrown', async (t) => {
+
+    const sheriff = new UrlSheriff({})
+
+    await assert.rejects(
+      sheriff.isSafe('http://localhost:3000'),
+      {
+        name: 'Error',
+        message: 'URL uses a private hostname'
+      }
+    )
+
+  })
+
+  test('If a URL uses a public hostname then it is allowed', async (t) => {
+    const sheriff = new UrlSheriff({})
+    const isSafe = await sheriff.isSafe('https://example.com')
+    assert.strictEqual(isSafe, true);
   })
 
 });
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,56 @@
-export async function sheriff(url: string): Promise<boolean> {
+import { URL } from 'url';
 
-  if (url.includes('localhost')) {
-    throw new Error('URL uses a host set to a localhost IP')
+export class UrlSheriff {
+  #config: object
+
+  #privateHostnames: string[] = [
+    'localhost',
+    '127',
+    '169'
+  ]
+
+  constructor(config: object) {
+    this.#config = config
   }
 
-  return true;
+  #getParsedUrl(url: string | URL): URL {
+    if (typeof url === 'string') {
+      return new URL(url)
+    }
+
+    if (url instanceof URL) {
+      return url
+    }
+
+    throw new Error('Invalid URL provided')
+  }
+
+  async isSafe(url: string | URL): Promise<boolean> {
+
+    let parsedUrl = this.#getParsedUrl(url)
+
+    const isPrivateHost = this.#privateHostnames.some((privateHostname) => {
+      return parsedUrl.hostname.startsWith(privateHostname)
+    })
+
+    if (isPrivateHost) {
+      throw new Error('URL uses a private hostname')
+    }
+
+    return true
+  }
 }
+
+/**
+ * Features to support:
+ * 1. add an allow-list of domains or ips that are allowed
+ * 1.1. domain list can be string literals or regex to match against
+ *
+ *
+ * Security controls:
+ * 1. runs IP address string matching (127.0.0.1 etc)
+ * 2. runs hostname string matching (localhost etc)
+ * 3. resolves the provided hostname to an IP address and runs the IP address string matching
+ * 4. check if IP is a public IP address namespace
+ * 5. a check that tests against DNS rebinding attacks
+ */
